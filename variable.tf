0ad3cdcbaab64ccf28ea455fc2fe46b03fe91521
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2024-07-23 17:54:19 +0200

    Update and rename variable.tf to variables.tf

--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "token" {
-  default  = ""
-}
-
-variable "ws_url" {
-  default  = "192.168.1.61:8163"
-}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "token" {
+  default  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMWU2NWFiOWIwY2Y0YWM4ODcwMTUyNTc5MDFlNjQzNiIsImlhdCI6MTcyMTc1MDAyMywiZXhwIjoyMDM3MTEwMDIzfQ.hg-gqd1osdFOkyVlsNUWzjQ3nEc9TsnlHBD5lKy33iI"
+}
+
+variable "ws_url" {
+  default  = "192.168.1.61:8163"
+}
