8c43ab4601c85da485c053469a88431f5205eb3f
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2024-07-23 21:05:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 # Pulls the image
-#resource "docker_image" "ha-linky" {
-#  name = "ubuntu:latest"
-#}
+resource "docker_image" "ha-linky" {
+  name = "ha-linky"
+}
 
 # Create a container
 resource "docker_container" "ha-linky" {
-  image   = docker_image.ha-linky.image_id
-  name    = "HA-Linky"
-  env {
-    SUPERVISOR_TOKEN = var.token
-    WS_URL           = ws://var.ws_url/api/websocket
-  }
+  image = docker_image.ha-linky.image_id
+  name  = "HA-Linky"
+  env   =["SUPERVISOR_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMWU2NWFiOWIwY2Y0YWM4ODcwMTUyNTc5MDFlNjQzNiIsImlhdC>
   volumes {
-    host_path        = /home/martial/linky
-    container_path   = /data
+    host_path      = "/home/martial/linky"
+    container_path = "/data"
   }
 }
+
