b376947fe9be1487fb1d5568a0da0499043a3774
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2024-07-24 10:08:29 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "token" {
-  default  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMWU2NWFiOWIwY2Y0YWM4ODcwMTUyNTc5MDFlNjQzNiIsImlhdCI6MTcyMTc1MDAyMywiZXhwIjoyMDM3MTEwMDIzfQ.hg-gqd1osdFOkyVlsNUWzjQ3nEc9TsnlHBD5lKy33iI"
+  description  = "Token généré via Conso API : https://conso.boris.sh/"
+  default      = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMWU2NWFiOWIwY2Y0YWM4ODcwMTUyNTc5MDFlNjQzNiIsImlhdCI6MTcyMTc1MDAyMywiZXhwIjoyMDM3MTEwMDIzfQ.hg-gqd1osdFOkyVlsNUWzjQ3nEc9TsnlHBD5lKy33iI"
 }
 
 variable "ws_url" {
-  default  = "192.168.1.61:8123"
+  description  = "Adresse IP de l'hôte Home assistant"
+  default      = "192.168.1.61:8123"
 }
