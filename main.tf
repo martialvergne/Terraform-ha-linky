54f5072c878026c4ad3aa9523bf275aeabbd9805
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2024-07-24 10:05:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "docker_image" "ha-linky" {
 resource "docker_container" "ha-linky" {
   image = docker_image.ha-linky.image_id
   name  = "HA-Linky"
-  env   = ["SUPERVISOR_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMWU2NWFiOWIwY2Y0YWM4ODcwMTUyNTc5MDFlNjQzNiIsImlhdCI6MTcyMTc1MDAyMywiZXhwIjoyMDM3MTEwMDIzfQ.hg-gqd1osdFOkyVlsNUWzjQ3nEc9TsnlHBD5lKy33iI","WS_URL=ws://192.168.1.61:8123/api/websocket"]
+  env   = ["SUPERVISOR_TOKEN=${var.token}","WS_URL=ws://${var.ws_url}/api/websocket"]
   volumes {
     host_path      = "/home/martial/linky"
     container_path = "/data"
